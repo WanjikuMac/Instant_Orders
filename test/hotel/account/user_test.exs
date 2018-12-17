d9af7e6ad859e43a56611b5fc4b9e1f2c525ea34
Wanjiku Macharia <wanjikumac@Carolines-MacBook-Pro.local>
2018-12-17 13:12:09 +0300

    added user tests

---
+++ test/hotel/account/user_test.exs
@@ -0,0 +1,37 @@
+defmodule Hotel.Account.UserTest do
+  @moduledoc """
+    This module contains unit tests for the user changesets
+  """
+  use Hotel.DataCase
+
+  alias Hotel.{
+    Account.User
+    }
+
+    @valid_user %{username: "WanjikuMac", password: "herself@123", email: "edwardscaroline32@gmail.com"}
+    @user_missing %{password: "herself@123", email: "edwardscaroline32@gmail.com"}
+    @pass_missing %{username: "WanjikuMac",email: "edwardscaroline32@gmail.com"}
+    @email_missing%{username: "WanjikuMac",password: "herself@123"}
+
+  describe "User changesets" do
+    test "validates a user login parameters" do
+        changeset = User.changeset(%User{}, @valid_user)
+        assert changeset.valid?
+      end
+
+      test "invalid if hash is missing" do
+        changeset = User.changeset(%User{},@pass_missing)
+        refute changeset.valid?
+      end
+
+      test "invalid if username is missing" do
+        changeset = User.changeset(%User{}, @user_missing)
+        refute changeset.valid?
+      end
+
+      test "invalid if email is missing" do
+        changeset = User.changeset(%User{}, @email_missing)
+        refute changeset.valid?
+    end
+  end
+end
