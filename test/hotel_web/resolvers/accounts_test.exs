96c5d934b8fa7e3775376af0d73ee4da9a3ac591
WanjikuMac <edwardscaroline32@gmail.com>
2018-12-31 17:13:18 +0300

    Log in reolver test

---
+++ test/hotel_web/resolvers/accounts_test.exs
@@ -0,0 +1,47 @@
+defmodule HotelWeb.Resolvers.AccountsTest do
+  use HotelWeb.ApiCase
+
+  alias Hotel.{
+    Accounts.User
+  }
+
+  describe " Accounts Resolver" do
+    test "log in a user to the system with a username", %{conn: conn} do
+      params = %{username: "WanjikuMac", password: "hash", email: "edwardscaroline32@gmail.com"}
+      {:ok, %User{} = user} = %User{} |> User.changeset(params) |> Repo.insert()
+
+      assert Repo.aggregate(User, :count, :id) == 1
+
+      variables = %{
+        "input" => %{
+          "sign_in_params" => user.username,
+          "password" => "hash"
+        }
+      }
+
+      query = """
+        mutation($input: SessionInput!){
+          sign_in(input: $input){
+            token
+            user{
+              username
+              email
+            }
+          }
+        }
+      """
+
+      res = post(conn, "api/graphiql", query: query, variables: variables)
+
+      %{
+        "data" => %{
+          "sign_in" => result
+        }
+      } = json_response(res, 200)
+
+      assert result["token"]
+      assert result["user"]["username"] == user.username
+      assert result["user"]["email"] == user.email
+    end
+  end
+end
