3b28a968541d5b551a39de23c9529b25bc2e4a52
WanjikuMac <edwardscaroline32@gmail.com>
2019-01-11 17:36:51 +0300

    add item table

---
+++ priv/repo/migrations/20190111111947_add_item.exs
@@ -0,0 +1,16 @@
+defmodule Hotel.Repo.Migrations.AddItem do
+  use Ecto.Migration
+  @doc """
+   Holds database entries for the items table
+  """
+  def change do
+    create table(:items, primary_key: :false) do
+      add :id , :binary_id, primary_key: :true
+      add :name, :string
+      add :descriotion, :string
+      add :price, :string
+
+      timestamps(inserted_at: :added_on, updated_at: :modified_on)
+    end
+  end
+end
