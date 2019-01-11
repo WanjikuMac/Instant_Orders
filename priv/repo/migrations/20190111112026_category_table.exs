02a5596ef818069d4376e741f4e2ce4932595160
WanjikuMac <edwardscaroline32@gmail.com>
2019-01-11 17:37:16 +0300

    add category table

---
+++ priv/repo/migrations/20190111112026_category_table.exs
@@ -0,0 +1,15 @@
+defmodule Hotel.Repo.Migrations.CategoryTable do
+  use Ecto.Migration
+  @doc """
+    This module holds entries for the category table
+  """
+  def change do
+    create table(:categories, primary_key: :false) do
+      add :id, :binary_id, primary_key: :true
+      add :name, :string
+      add :description, :string
+
+      timestamps(inserted_at: :added_on, updated_at: :modifieed_on)
+    end
+  end
+end
