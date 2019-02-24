91fffca5994ad5d08f58f7c027d5272a2beb39c1
WanjikuMac <edwardscaroline32@gmail.com>
2019-02-24 21:50:59 +0300

    Renamed the description field

--- priv/repo/migrations/20190111111947_add_item.exs
+++ priv/repo/migrations/20190111111947_add_item.exs
@@ -7,7 +7,7 @@ defmodule Hotel.Repo.Migrations.AddItem do
     create table(:items, primary_key: :false) do
       add :id , :binary_id, primary_key: :true
       add :name, :string
-      add :descriotion, :string
+      add :description, :string
       add :price, :string
 
       timestamps(inserted_at: :added_on, updated_at: :modified_on)
